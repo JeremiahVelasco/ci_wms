336a89c231edf10e8b86781d37bdcc771d19dff8
JeremiahVelasco <velascojeremiahd@gmail.com>
2026-01-21 08:13:46 -0700

    fix: removed temp routes

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Artisan;
-
-Route::get('/sym-link', function () {
-    Artisan::call('storage:link');
-    return 'Storage linked successfully';
-});
-
-Route::get('/shield-generate', function () {
-    Artisan::call('shield:generate', [
-        '--all' => true,
-        '--no-interaction' => true,
-    ]);
-    return 'Shield generated successfully';
-});
-
-Route::get('/shield-super-admin', function () {
-    Artisan::call('shield:super-admin', [
-        '--user' => 1,
-        '--no-interaction' => true,
-    ]);
-    return 'Shield super admin generated successfully';
-});
