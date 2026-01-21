26e99226684758a4156a77fb0c07b0b372d20883
JeremiahVelasco <velascojeremiahd@gmail.com>
2026-01-21 08:04:13 -0700

    feat: new routes

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/sym-link', function () {
+    Artisan::call('storage:link');
+    return 'Storage linked successfully';
+});
+
+Route::get('/shield-generate', function () {
+    Artisan::call('shield:generate --all');
+    return 'Shield generated successfully';
+});
+
+Route::get('/shield-super-admin', function () {
+    Artisan::call('shield:super-admin 1');
+    return 'Shield super admin generated successfully';
+});
