d42268a35d3c706a23f69af75187e480f3402262
JeremiahVelasco <velascojeremiahd@gmail.com>
2026-01-21 08:07:24 -0700

    fix: routes

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,17 @@ Route::get('/sym-link', function () {
 });
 
 Route::get('/shield-generate', function () {
-    Artisan::call('shield:generate --all');
+    Artisan::call('shield:generate', [
+        '--all' => true,
+        '--no-interaction' => true,
+    ]);
     return 'Shield generated successfully';
 });
 
 Route::get('/shield-super-admin', function () {
-    Artisan::call('shield:super-admin 1');
+    Artisan::call('shield:super-admin', [
+        '--user' => 1,
+        '--no-interaction' => true,
+    ]);
     return 'Shield super admin generated successfully';
 });
